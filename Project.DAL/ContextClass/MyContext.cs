49dc47b04793ff39bc108c4823d43ac5f28dbe0b
kurumsal <kurumsal@KDK-102-PC08-YZ>
2020-11-02 13:04:07 +0300

    Dal islemleri yapildi

---
+++ Project.DAL/ContextClass/MyContext.cs
@@ -0,0 +1,26 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClass
+{
+   public class MyContext : DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+      
+    }
+}
---
+++ WinUI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
