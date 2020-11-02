c64446f994b013abe7542a0bb3c425e0767a1872
Arif Gündoğdu <gundogduarif4@gmail.com>
2020-11-02 13:07:36 +0300

    tamamlandı bll

---
+++ Project.BLL/DesignPattern/GenericRepository/BaseRep/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Project.BLL.DesignPattern.SingletonPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPattern.GenericRepository.BaseRep
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        MyContext _db;
+
+        public BaseRepository()
+        {
+            _db = DbTool.DBInstance;
+        }
+        private void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Project.BLL/DesignPattern/GenericRepository/ConstRep/UserProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPattern.GenericRepository.ConstRep
+{
+    public class UserProfileRepository:BaseRepository<UserProfile>
+    {
+    }
+}
